1587fde5aa71b63bcc174fa0fa050356f5480234
Edwin Immanuel Damanik <58164595+edwindamanik@users.noreply.github.com>
2023-03-02 11:26:33 +0700

    Update ArticleCommentController.php

--- app/Http/Controllers/Admin/ArticleCommentController.php
+++ app/Http/Controllers/Admin/ArticleCommentController.php
@@ -24,20 +24,6 @@ class ArticleCommentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -92,40 +78,6 @@ class ArticleCommentController extends Controller
         }
 
         return redirect()->back()->with('success', 'Komentar Artikel Berhasil Dibuat!');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
 
     /**
